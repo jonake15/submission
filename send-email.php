28a35b4e62f70afb7b51d295d764926287fb54ab
jonake15 <113668172+jonake15@users.noreply.github.com>
2024-03-06 17:13:38 +0300

    first-commit

---
+++ send-email.php
@@ -0,0 +1,16 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+
+?>
